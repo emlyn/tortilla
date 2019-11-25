e9dd877408f32d7e674cc910e6c5149106b3107b
Emlyn Corrin <emlyn.corrin@gmail.com>
2019-11-25 14:28:33 +0000

    Update project url

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject tortilla "0.1.0-SNAPSHOT"
   :description "A thin wrapper for accessing Java classes from Clojure"
-  :url "http://example.com/FIXME"
+
+  :url "https://github.com/emlyn/tortilla"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
