4b71fcf9aa2fb662d2fed7d719db5faafcdd5bca
Emlyn Corrin <emlyn.corrin@gmail.com>
2019-11-25 22:38:20 +0000

    Use kaocha for tests

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
 
   :url "https://github.com/emlyn/tortilla"
 
+  :scm {:name "git"
+        :url "https://github.com/emlyn/tortilla"}
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -12,9 +15,15 @@
 
   :repl-options {:init-ns tortilla.wrap-test}
 
+  :deploy-branches ["master"]
+
+  :aliases
+  {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
+
   :profiles
   {:provided
    {:dependencies [[org.clojure/clojure "1.10.1"]]}
 
-   :test
-   {:java-source-paths ["test/java"]}})
+   :kaocha
+   {:dependencies [[lambdaisland/kaocha "0.0-554"]]
+    :java-source-paths ["test/java"]}})
