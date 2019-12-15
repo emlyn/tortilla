531ec27a3ceb8d3caa9fc5719137909b296e39f8
Emlyn Corrin <emlyn.corrin@gmail.com>
2019-12-15 11:34:11 +0000

    Fix tests that look at text output
    In a previous commit I had added `;;` to the start of non-code lines so that the output should be valid Clojure code.
    This just updates the tests to take that into account.

--- src/test/tortilla/main_test.clj
+++ src/test/tortilla/main_test.clj
@@ -4,10 +4,10 @@
 
 (deftest main-test
   (let [stdout (with-out-str (m/-main "Object" "java.lang.Number"))]
-    (is (re-find #"(?m)^=+ Object =+$" stdout))
-    (is (re-find #"(?m)^=+ java.lang.Number =+$" stdout))
+    (is (re-find #"(?m)^;; =+ Object =+$" stdout))
+    (is (re-find #"(?m)^;; =+ java.lang.Number =+$" stdout))
     (is (re-find #"(?m)^ \(clojure.core/defn" stdout))
     (is (re-find #"(?m)\bint-value\b" stdout)))
   (let [stdout (with-out-str (m/-main "invalid.missing.NonexistentClass_"))]
-    (is (re-find #"(?m)^=+ invalid.missing.NonexistentClass_ =+$" stdout))
-    (is (re-find #"(?m)^Error loading class$" stdout))))
+    (is (re-find #"(?m)^;; =+ invalid.missing.NonexistentClass_ =+$" stdout))
+    (is (re-find #"(?m)^;; Error loading class$" stdout))))
