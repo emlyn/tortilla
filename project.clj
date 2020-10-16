5cc3295a5cc5ddf30f751f3e62d1c3c268a1a789
Emlyn Corrin <emlyn.corrin@gmail.com>
2020-10-16 23:50:39 +0100

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emlyn/tortilla "0.1.3-SNAPSHOT"
+(defproject emlyn/tortilla "0.1.3"
   :description "A thin wrapper for accessing Java classes from Clojure"
 
   :url "https://github.com/emlyn/tortilla"
